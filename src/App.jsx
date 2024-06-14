4f61c80a5c42606be057908893024c60ade972f2
MaiLakshyaHoon <lakshyamaharshi007@gmail.com>
2024-06-15 03:41:02 +0530

    Nav-bar added

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Navbar from './components/Navbar.jsx'
+
+function App() {
+
+  return (
+    <div className="app-main-div">
+      <Navbar/>
+    </div>
+  )
+}
+
+export default App
